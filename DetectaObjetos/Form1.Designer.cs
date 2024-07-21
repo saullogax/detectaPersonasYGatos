6fc4065cf271a0a07576a228823024445de99a00
Guillermo Gaxiola <saullogax@gmail.com>
2024-07-20 18:24:36 -0700

    detecta personas y gatos

---
+++ DetectaObjetos/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DetectaObjetos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnCargaImagen = new System.Windows.Forms.Button();
+            this.btnCargaImagenProcesaPersonas = new System.Windows.Forms.Button();
+            this.btnCargaImagenProcesaGatos = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCargaImagen
+            // 
+            this.btnCargaImagen.Location = new System.Drawing.Point(12, 406);
+            this.btnCargaImagen.Name = "btnCargaImagen";
+            this.btnCargaImagen.Size = new System.Drawing.Size(415, 67);
+            this.btnCargaImagen.TabIndex = 2;
+            this.btnCargaImagen.Text = "CARGAR IMAGEN";
+            this.btnCargaImagen.UseVisualStyleBackColor = true;
+            this.btnCargaImagen.Click += new System.EventHandler(this.btnCargaImagen_Click);
+            // 
+            // btnCargaImagenProcesaPersonas
+            // 
+            this.btnCargaImagenProcesaPersonas.Location = new System.Drawing.Point(433, 406);
+            this.btnCargaImagenProcesaPersonas.Name = "btnCargaImagenProcesaPersonas";
+            this.btnCargaImagenProcesaPersonas.Size = new System.Drawing.Size(207, 67);
+            this.btnCargaImagenProcesaPersonas.TabIndex = 3;
+            this.btnCargaImagenProcesaPersonas.Text = "CARGA IMAGEN Y PROCESA PERSONAS";
+            this.btnCargaImagenProcesaPersonas.UseVisualStyleBackColor = true;
+            this.btnCargaImagenProcesaPersonas.Click += new System.EventHandler(this.btnCargaImagenProcesaPersonas_Click);
+            // 
+            // btnCargaImagenProcesaGatos
+            // 
+            this.btnCargaImagenProcesaGatos.Location = new System.Drawing.Point(658, 406);
+            this.btnCargaImagenProcesaGatos.Name = "btnCargaImagenProcesaGatos";
+            this.btnCargaImagenProcesaGatos.Size = new System.Drawing.Size(199, 67);
+            this.btnCargaImagenProcesaGatos.TabIndex = 4;
+            this.btnCargaImagenProcesaGatos.Text = "CARGA IMAGEN Y PROCESA GATOS";
+            this.btnCargaImagenProcesaGatos.UseVisualStyleBackColor = true;
+            this.btnCargaImagenProcesaGatos.Click += new System.EventHandler(this.btnCargaImagenProcesaGatos_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 12);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(415, 388);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(433, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(424, 388);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 485);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.btnCargaImagenProcesaGatos);
+            this.Controls.Add(this.btnCargaImagenProcesaPersonas);
+            this.Controls.Add(this.btnCargaImagen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCargaImagen;
+        private System.Windows.Forms.Button btnCargaImagenProcesaPersonas;
+        private System.Windows.Forms.Button btnCargaImagenProcesaGatos;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+    }
+}
+
---
+++ DetectaObjetos/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Threading;
+
+
+namespace DetectaObjetos
+{
+    public partial class Form1 : Form
+    {
+        private CascadeClassifier _CascadeClassifier;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            _CascadeClassifier = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalface_default.xml");
+            _CascadeClassifier1 = new CascadeClassifier(Application.StartupPath + "/haarcascade_frontalcatface_extended.xml");
+        }
+
+        private void btnCargaImagen_Click(object sender, EventArgs e)
+        {
+           
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> imagen = new Image<Bgr, byte>(open.FileName);
+                imageBox1.Image = imagen;
+                using (var imageFrame = imagen)
+                {
+                    if (imageFrame != null)
+                    {
+                        var grayFrame = imageFrame.Convert<Bgr, Byte>();
+                       
+                       
+
+                    }
+                }
+            }
+        }
+
+        private void btnCargaImagenProcesaPersonas_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> imagen = new Image<Bgr, byte>(open.FileName);
+                imageBox2.Image = imagen;
+                using (var imageFrame = imagen)
+                {
+                    if (imageFrame != null)
+                    {
+                        var grayFrame = imageFrame.Convert<Bgr, Byte>();
+                        var faces = _CascadeClassifier.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+                        
+
+
+                        foreach (var face in faces)
+                        {
+                            imageFrame.Draw(face, new Bgr(Color.Green), 3);
+                            
+                        }
+                    }
+                }
+            }
+        }
+        private CascadeClassifier _CascadeClassifier1;
+        private void btnCargaImagenProcesaGatos_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                Image<Bgr, Byte> imagen = new Image<Bgr, byte>(open.FileName);
+                imageBox2.Image = imagen;
+                using (var imageFrame = imagen)
+                {
+                    if (imageFrame != null)
+                    {
+                        var grayFrame = imageFrame.Convert<Bgr, Byte>();
+                        var cats = _CascadeClassifier1.DetectMultiScale(grayFrame, 1.1, 10, Size.Empty);
+
+
+
+                        foreach (var cat in cats)
+                        {
+                            imageFrame.Draw(cat, new Bgr(Color.Blue), 3);
+
+                        }
+                    }
+                }
+            }
+        }
+
+        }
+    }
+
